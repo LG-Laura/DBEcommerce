ecec339b60630fc1c066eda5a04a829a3d2022d6
Laura Lopez <lalopezgar@gmail.com>
2024-08-29 15:17:56 -0300

    Agregación del contexto

---
+++ Data/backendContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class backendContext : DbContext
+{
+    public backendContext(DbContextOptions<backendContext> options) : base(options) { }
+
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Producto> Productos { get; set; }
+    public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<Venta> Ventas { get; set; }
+    public DbSet<DetalleVenta> DetalleVentas { get; set; }
+}
+
